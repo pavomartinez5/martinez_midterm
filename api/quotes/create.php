8bfa5b9f13d2a39b3108797d3cac9124e3315f16
Pavel Martinez <pavomartinez@hotmail.com>
2025-03-16 18:15:43 -0700

    testing

--- api/quotes/create.php
+++ api/quotes/create.php
@@ -46,7 +46,7 @@
         //Convert to  JSON
         print_r(json_encode($quote_arr));
     } else {
-        if($this->author_id){
+        /* if($this->author_id){
             echo json_encode(
                 array('message' => 'author_id Not Found')
                 );
@@ -58,6 +58,11 @@
         echo json_encode(
         array('message' => 'No Quotes created')
         );
-     }
+     } */
+
+
+     echo json_encode(
+        array('message' => 'No Quotes created')
+        );
     }
 ?>
