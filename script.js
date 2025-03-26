930270073d437881c16e88f2c700de83821daf24
Pavel Martinez <pavomartinez@hotmail.com>
2025-03-26 01:46:57 -0700

    updated random

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ function generateQuotes() {
   quote.innerHTML = `Loading New Quotes...<i class = "fa-solid fa-sync fa-spin"></i>`;
   generate.innerHTML = "Generating...";
 
-  fetch("../martinez_midterm/api/authors/read.php")
+  fetch("../martinez_midterm/api/authors/read_single.php/?random=true")
     .then((response) => response.json())
     .then((data) => {
       console.log(data);
